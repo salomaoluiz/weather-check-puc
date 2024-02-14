1e7fec56cefab35e088d35324b42f6a3c341d700
salomao-neto <salomao.cruz.15@gmail.com>
2024-02-14 16:48:54 -0300

    feat: updated the position repository

--- lib/domain/repository/position_repository.dart
+++ lib/domain/repository/position_repository.dart
@@ -1,7 +1,12 @@
 import 'package:check_weather/core/infra/api_result_model.dart';
-import 'package:check_weather/domain/entity/position_entity.dart';
+import 'package:check_weather/domain/entity/route_entity.dart';
+import 'package:check_weather/domain/entity/weather.dart';
 import 'package:check_weather/domain/repository/request/position_repository_request.dart';
 
 abstract class PositionRepository {
-  Future<ApiResultModel<List<PositionEntity>>> getPositions(GetPositionsRequest request);
+  Future<ApiResultModel<List<RouteEntity>>> getRoute(
+      GetPositionsRequest request);
+
+  Future<ApiResultModel<List<Weather>>> getWeatherByRoute(
+      GetWeatherByPositionsRequest request);
 }
