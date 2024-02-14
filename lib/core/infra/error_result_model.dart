cb90142046887f1ddf8dcdc0b09b1cbf598a7101
salomao-neto <salomao.cruz.15@gmail.com>
2024-02-14 14:10:30 -0300

    refactor: removed equatable

--- lib/core/infra/error_result_model.dart
+++ lib/core/infra/error_result_model.dart
@@ -1,11 +1,6 @@
-import 'package:equatable/equatable.dart';
-
-class ErrorResultModel extends Equatable {
+class ErrorResultModel {
   const ErrorResultModel({this.statusCode, this.message});
 
   final int? statusCode;
   final String? message;
-
-  @override
-  List<Object?> get props => <Object?>[statusCode, message];
 }
