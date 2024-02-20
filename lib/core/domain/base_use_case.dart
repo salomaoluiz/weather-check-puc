a2e3a4b1e1bb398151bebf4a354f55220cead776
salomao-neto <salomao.cruz.15@gmail.com>
2024-02-20 20:18:16 -0300

    refactor: removed freezed

--- lib/core/domain/base_use_case.dart
+++ lib/core/domain/base_use_case.dart
@@ -1,9 +1,8 @@
-import 'package:check_weather/core/infra/api_result_model.dart';
 
 abstract class BaseParamsUseCase<Type, Params> {
-  Future<ApiResultModel<Type>> call(Params params);
+  Future<Type> call(Params params);
 }
 
 abstract class NoParamsUseCase<Type> {
-  Future<ApiResultModel<Type>> call();
+  Future<Type> call();
 }
--- lib/domain/repository/position_repository.dart
+++ lib/domain/repository/position_repository.dart
@@ -1,12 +1,11 @@
-import 'package:check_weather/core/infra/api_result_model.dart';
 import 'package:check_weather/domain/entity/route_entity.dart';
 import 'package:check_weather/domain/entity/weather.dart';
 import 'package:check_weather/domain/repository/request/position_repository_request.dart';
 
 abstract class PositionRepository {
-  Future<ApiResultModel<RouteEntity>> getRoute(
+  Future<RouteEntity> getRoute(
       GetPositionsRequest request);
 
-  Future<ApiResultModel<List<WeatherEntity>>> getWeatherByRoute(
+  Future<List<WeatherEntity>> getWeatherByRoute(
       GetWeatherByPositionsRequest request);
 }
