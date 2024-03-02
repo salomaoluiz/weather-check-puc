5b606bdb55d7d439d92c93434723f944d852cb39
salomao-neto <salomao.cruz.15@gmail.com>
2024-03-02 14:40:35 -0300

    feat: added a request to get the weather route

--- lib/infra/data/datasource/azure_data_source.dart
+++ lib/infra/data/datasource/azure_data_source.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 
 import 'package:check_weather/infra/data/model/route_directions/route_directions.dart';
+import 'package:check_weather/infra/data/model/weather_route/weather_route.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 
 class GetRouteDirectionsParamethers {
   String position;
+
   GetRouteDirectionsParamethers({required this.position});
 }
 
@@ -33,5 +35,21 @@ class AzureDataSource {
     var result = await httpClient.get(url);
 
     return RouteDirectionsModel.fromJSON(jsonDecode(result.body));
+  }
+
+  Future<WeatherRouteModel> getWeatherRoute(String position) async {
+    final queryParamethers = {
+      'api-version': '1.1',
+      'query': position,
+      'subscription-key': dotenv.get('SUBSCRIPTION-KEY'),
+      'language': 'pt-BR',
+    };
+
+    var url = Uri.https(
+        'atlas.microsoft.com', 'weather/route/json', queryParamethers);
+
+    var result = await httpClient.get(url);
+
+    return WeatherRouteModel.fromJSON(jsonDecode(result.body));
   }
 }
--- test/infra/data/datasource/azure_data_source_test.dart
+++ test/infra/data/datasource/azure_data_source_test.dart
@@ -1,11 +1,13 @@
-import 'package:check_weather/infra/data/datasource/get_route_directions.dart';
+import 'package:check_weather/infra/data/datasource/azure_data_source.dart';
 import 'package:check_weather/infra/data/model/route_directions/route_directions.dart';
+import 'package:check_weather/infra/data/model/weather_route/weather_route.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 
 import '../../mocks/route_directions_mock.dart';
+import '../../mocks/weather_route_mock.dart';
 
 class HTTPMock extends Mock implements http.Client {}
 
@@ -17,7 +19,9 @@ void main() {
     registerFallbackValue(HTTPMock());
     registerFallbackValue(UriMock());
   });
-  test('should request the Azure API with the correct params', () async {
+  test(
+      'should request the Azure API with the correct params and return a RouteDirectionsModel',
+      () async {
     HTTPMock httpClient = HTTPMock();
     AzureDataSource dataSource = AzureDataSource(httpClient: httpClient);
 
@@ -44,5 +48,31 @@ void main() {
 
     verify(() => httpClient.get(url)).called(1);
     expect(result, isA<RouteDirectionsModel>());
+  });
+
+  test(
+      'should request the Azure API with the correct params and return a WeatherRouteModel',
+      () async {
+    HTTPMock httpClient = HTTPMock();
+    AzureDataSource dataSource = AzureDataSource(httpClient: httpClient);
+
+    when(() => httpClient.get(any()))
+        .thenAnswer((_) async => http.Response(weatherRouteMock, 200));
+
+    var result = await dataSource
+        .getWeatherRoute("-23.397388,-51.926810,0:-25.454850,-49.279046,0");
+
+    final queryParamethers = {
+      'api-version': '1.1',
+      'query': "-23.397388,-51.926810,0:-25.454850,-49.279046,0",
+      'subscription-key': "MOCKED_SUBSCRIPTION_KEY",
+      'language': 'pt-BR',
+    };
+
+    var url = Uri.https(
+        'atlas.microsoft.com', 'weather/route/json', queryParamethers);
+
+    verify(() => httpClient.get(url)).called(1);
+    expect(result, isA<WeatherRouteModel>());
   });
 }
