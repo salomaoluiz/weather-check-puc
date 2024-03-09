7ccc4dde4ed0f16e44f8cdeb27167d4a41a23718
salomao-neto <salomao.cruz.15@gmail.com>
2024-03-09 14:23:06 -0300

    refactor: added default value to travelTimeInSeconds

--- lib/domain/entity/position_entity.dart
+++ lib/domain/entity/position_entity.dart
@@ -2,11 +2,11 @@ class PositionEntity {
   const PositionEntity(
       {required this.latitude,
       required this.longitude,
-      this.travelTimeInSeconds,
+      this.travelTimeInSeconds = 0,
       this.distanceInMeters});
 
   final double latitude;
   final double longitude;
-  final int? travelTimeInSeconds;
+  final int travelTimeInSeconds;
   final int? distanceInMeters;
 }
